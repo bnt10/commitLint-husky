e2efbca4603dad008e96182568b295cb6f4f3ba0
jin <bnt10@naver.com>
2023-07-01 16:02:12 +0900

    ci(lint-staged-config) 검사 방식 변경

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "eslint"],
-  "**/*.ts?(x)": () => "npm run check-types",
-  "*.{json,yaml}": ["prettier --write"],
-};
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '**/*.ts?(x)': filenames => [
+    ...filenames.map(filename => `eslint --fix '${filename}'`),
+    ...filenames.map(filename => `bash -c tsc --noEmit --pretty '${filename}'`),
+  ],
+  '*.{json,yaml}': ['prettier --write'],
+}
